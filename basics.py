068534985979c97ec747e6e1700e4ab13ce2a289
Leonela Baez <bleonela45@gmail.com>
2026-01-07 17:53:20 -0300

    first python basics

---
+++ basics.py
@@ -0,0 +1,16 @@
+#variables
+name = "Leonela"
+age = 19
+
+print ("name: ", name)
+print ("age: ", age)
+
+#condition
+if age >= 18:
+    print ("adult")
+else:
+    print ("minor")
+
+#loop
+for i in range(5):
+    print ("number: ", i)
