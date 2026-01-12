194b282374ea5fe699a6c3cdb1bdc3a61859c5fc
Leonela Baez <bleonela45@gmail.com>
2026-01-12 18:14:51 -0300

    Actualizar basics.py

--- basics.py
+++ basics.py
@@ -10,7 +10,13 @@ if age >= 18:
     print ("adult")
 else:
     print ("minor")
+if age = minor:
+    print ("age leagaly")
+ese:
+    print ("denegado")
 
 #loop
 for i in range(5):
+    print ("number: ", i)
+for i un range(10):
     print ("number: ", i)
