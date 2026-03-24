dc64b1153513cd0952a254bcedd3ac7791a3402c
Leonela Baez <bleonela45@gmail.com>
2026-03-23 21:47:06 -0300

    Actualizar loops.py

--- loops.py
+++ loops.py
@@ -28,4 +28,6 @@ my man is so lovely, hot, nice, inv
 
 podrad??
 
-podremos, lo haremos. Que no nos importe el resto
+podremos, lo haremos. Que no nos importe el
+
+Haremos lo posible, y solo lo posible.  resto
